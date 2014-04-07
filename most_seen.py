5cdd21b1c612e7e96da6e21a557ee9b7de2ed894
usuario <usuario@debian.gonzalonazareno.org>
2014-04-07 10:23:18 +0100

    series mas vistas

--- most_seen.py
+++ most_seen.py
@@ -9,4 +9,9 @@ auth_token = fichero.readline()
 q = {'auth_token':auth_token}
 r = requests.get('http://api.series.ly/v2/media/most_seen',params=q)
 jresp = json.loads(r.text)
-respuesta.write(r.text)
+
+#print jresp['series'][0]
+#respuesta.write(json.dumps(jresp['series'][0]))
+
+for i in jresp['series']:
+	print i['name']
