9b6e98ca92b648fc992f854f213a7ffc213384ba
FranBarrera6 <franbarrera6@gmail.com>
2014-04-10 19:09:32 +0200

    bottle

--- user_token.py
+++ user_token.py
@@ -1,9 +1,13 @@
 import requests
 from getpass import getpass
 import json
+from bottle import route, run
+
+
 
 fichero = open('auth.txt','r')
 auth_token = fichero.readline()
+fi_usertoken = open('user_token.txt','w')
 
 
 def fuser_token():
@@ -12,6 +16,8 @@ def fuser_token():
 	q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
 	r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
 	jresp = json.loads(r.text)
+	token = jresp['user_token']
+	fi_usertoken.write(token)
 	return jresp['user_token']
 
 
@@ -20,10 +26,25 @@ def fseriesfollowing():
 	q_sf = {'auth_token':auth_token,'user_token':user_token}
 	r_sf = requests.get('http://api.series.ly/v2/user/media/series',params=q_sf)
 	jresp = json.loads(r_sf.text)
-	for i in jresp['series']:
-		print i['name']
+	return jresp
 
 prueba = fseriesfollowing()
+
+
+from bottle import route, run, debug, request
+
+@route('/')
+def test():
+	for i in prueba:
+		return i['name']
+
+run()
+
+
+
+
+
+
 
 fichero.close()
 
