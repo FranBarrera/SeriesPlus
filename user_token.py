78fee27686cc75e40f778552469ca3d26735ff68
usuario <usuario@debian.gonzalonazareno.org>
2014-04-10 09:10:05 +0100

    imprimir user_token

--- user_token.py
+++ user_token.py
@@ -10,3 +10,4 @@ password = getpass('Password')
 q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
 r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
 
+print r.text
