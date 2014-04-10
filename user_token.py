60a4f8178b9b2612263abb83a7258e0a6216c646
usuario <usuario@debian.gonzalonazareno.org>
2014-04-10 09:08:40 +0100

    login por consola

---
+++ user_token.py
@@ -0,0 +1,12 @@
+import requests
+from getpass import getpass
+
+
+fichero = open('auth.txt','r')
+auth_token = fichero.readline()
+username = raw_input('Username')
+password = getpass('Password')
+
+q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
+r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
+
