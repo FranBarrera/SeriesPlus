da13d321a3867f484b810f9f507d6fe79c4395e9
FranBarrera6 <franbarrera6@gmail.com>
2014-04-10 17:47:43 +0200

    añadir funcion series que sigue

--- user_token.py
+++ user_token.py
@@ -7,9 +7,16 @@ auth_token = fichero.readline()
 username = raw_input('Username')
 password = getpass('Password')
 
+
 q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
 r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
 
 jresp = json.loads(r.text)
 
-print jresp['user_token']
+user_token = jresp['user_token']
+
+def seriesfollowing():
+	q_sf = {'auth_token':auth_token,'user_token':user_token}
+	r_sf = requests.get('http://api.series.ly/v2/user/media/series',params=q_sf)
+	jresp = json.loads(r_sf.text)
+	return jresp
