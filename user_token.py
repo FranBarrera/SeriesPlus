4aa9399c4632ce81339536c410a0aa94b1edf3fd
FranBarrera6 <franbarrera6@gmail.com>
2014-04-10 18:12:45 +0200

    añadir funcion para sacar token

--- user_token.py
+++ user_token.py
@@ -4,19 +4,28 @@ import json
 
 fichero = open('auth.txt','r')
 auth_token = fichero.readline()
-username = raw_input('Username')
-password = getpass('Password')
+
+def fuser_token():
+	username = raw_input('Username')
+	password = getpass('Password')
+	q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
+	r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
+	jresp = json.loads(r.text)
+	user_token = jresp['user_token']
+	#fichero_token.write(user_token)
+	print user_token
+
+usertoken = fuser_token()
+print usertoken
 
 
-q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
-r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
+#def fseriesfollowing():
+#	q_sf = {'auth_token':auth_token,'user_token':user_token}
+#	r_sf = requests.get('http://api.series.ly/v2/user/media/series',params=q_sf)
+#	jresp = json.loads(r_sf.text)
+#	print r_sf.text
 
-jresp = json.loads(r.text)
+#prueba = fseriesfollowing
+#print prueba
 
-user_token = jresp['user_token']
-
-def seriesfollowing():
-	q_sf = {'auth_token':auth_token,'user_token':user_token}
-	r_sf = requests.get('http://api.series.ly/v2/user/media/series',params=q_sf)
-	jresp = json.loads(r_sf.text)
-	return jresp
+fichero.close()
