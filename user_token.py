50a4679a9916ae870ca34cad0e32d392c65606d0
FranBarrera6 <franbarrera6@gmail.com>
2014-04-10 18:26:05 +0200

    sacar series que sigue

--- user_token.py
+++ user_token.py
@@ -7,8 +7,8 @@ auth_token = fichero.readline()
 
 
 def fuser_token():
-	username = raw_input('Username')
-	password = getpass('Password')
+	username = raw_input('Username: ')
+	password = getpass('Password: ')
 	q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
 	r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
 	jresp = json.loads(r.text)
@@ -20,10 +20,10 @@ def fseriesfollowing():
 	q_sf = {'auth_token':auth_token,'user_token':user_token}
 	r_sf = requests.get('http://api.series.ly/v2/user/media/series',params=q_sf)
 	jresp = json.loads(r_sf.text)
-	print r_sf.text
+	for i in jresp['series']:
+		print i['name']
 
 prueba = fseriesfollowing()
-print prueba
 
 fichero.close()
 
