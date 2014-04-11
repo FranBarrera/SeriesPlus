613b5c28776dcf602dbee96b8b4d5bc42280cc97
usuario <usuario@debian.gonzalonazareno.org>
2014-04-11 10:24:20 +0100

    definir login con bottle

--- user_token.py
+++ user_token.py
@@ -1,7 +1,7 @@
 import requests
 from getpass import getpass
 import json
-from bottle import route, run, template
+from bottle import route, run, template, get, post
 
 
 fichero = open('auth.txt','r')
@@ -10,16 +10,18 @@ respuesta = open('most_seen.txt','w')
 fi_usertoken = open('user_token.txt','w')
 
 
-def fuser_token():
-	username = raw_input('Username: ')
-	password = getpass('Password: ')
-	q = {'auth_token':auth_token,'username':username,'password':password,'remember':'0'}
+def fuser_token(username, password):
+#	username = raw_input('Username: ')
+#	password = getpass('Password: ')
+	q = {'auth_token':auth_token,'username':username,'password':password,'remember':'1'}
 	r = requests.get('http://api.series.ly/v2/user/user_token',params=q)
-	jresp = json.loads(r.text)
-	token = jresp['user_token']
-	fi_usertoken.write(token)
-	return jresp['user_token']
-
+	if r.status_code == 200:
+		jresp = json.loads(r.text)
+		token = jresp['user_token']
+		#fi_usertoken.write(token)
+		return token
+	else:
+		return ''	
 
 def fseriesfollowing():
 	user_token = fuser_token()
@@ -44,11 +46,37 @@ nombres_mostseen = []
 for i in seriesmost['series']:
 	nombres_mostseen.append(i['name'])
 
-@route('/')
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+	token1 = fuser_token(username,password)
+    if len(token1) > 0:
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+
+
+@route('/template')
 def test():
 	return template('template.tpl',nombres=nombres,nombres_mostseen=nombres_mostseen)
-
 run(host='localhost', port=8080)
 
+
+
 fichero.close()
+respuesta.close()
+fi_usertoken.close()
 
