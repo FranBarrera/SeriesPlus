096773007b6fe1f3a1458a2ac8650955fe2a8067
FranBarrera6 <franbarrera6@gmail.com>
2014-04-14 23:12:05 +0200

    añadir seasons desordenadas

--- serie.tpl
+++ serie.tpl
@@ -3,7 +3,7 @@
 	</head>
 	<body>
 		<h1> {{data_raw['name']}} </h1>
-		#%for i in data_raw['seasons']:
-			<h3>#{{i}}</h3>
+		%for i in data_raw['seasons_episodes']:
+			<h3>{{i}}</h3>
 	</body>
 </html>
--- user_token.py
+++ user_token.py
@@ -25,6 +25,7 @@ def full_info(user_token,idm,mediaType):
 	q_sf = {'auth_token':auth_token,'user_token':user_token,'idm':idm,'mediaType':mediaType}
 	r_sf = requests.get('http://api.series.ly/v2/media/full_info',params=q_sf)
 	jresp = json.loads(r_sf.text)
+	print jresp
 	return jresp
 
 fichero.close()
