11155084d07c9dae931a408f4d19604915fdb75b
FranBarrera6 <franbarrera6@gmail.com>
2014-04-23 23:55:24 +0200

    añadir link a cada tipo de media

--- mybottle.py
+++ mybottle.py
@@ -2,6 +2,10 @@ from bottle import route, run, template, get, post, request, response, redirect
 from user_token import fuser_token
 from user_token import fseriesfollowing,full_info
 
+@get('/peliculas/:idm')
+def le_pelicula(idm):
+    mediaType = 2
+    return template('pelicula.tpl',data_raw=full_info(request.get_cookie("user_token"),idm,mediaType))
 
 @get('/main') # or @route('/login')
 def le_main():
@@ -12,10 +16,10 @@ def le_serie(idm):
     mediaType = 1
     return template('serie.tpl',data_raw=full_info(request.get_cookie("user_token"),idm,mediaType))
 
-@get('/login') # or @route('/login')
+@get('/') # or @route('/')
 def login():
     return '''
-        <form action="/login" method="post">
+        <form action="/" method="post">
             Username: <input name="username" type="text" />
             Password: <input name="password" type="password" />
             <input value="Login" type="submit" />
@@ -23,7 +27,7 @@ def login():
     '''
 
 
-@post('/login') # or @route('/login', method='POST')
+@post('/') # or @route('/', method='POST')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
--- template.tpl
+++ template.tpl
@@ -7,13 +7,26 @@
 		%for data in data_raw:
 			%if data!= 'error':
 				%if len(data_raw[data]) > 0:
+
 					<h2> {{data}} </h2>
 					<ul>
+						%tipo = ''
 						%for media in data_raw[data]:
-							<li><a href="serie/{{media['idm']}}">{{media['name']}}</a></li>
+						%if media['mediaType'] == 1:
+							%tipo = 'serie'
+						%elif media['mediaType'] == 2:
+							%tipo = 'peliculas'
+						%elif media['mediaType'] == 3:
+							%tipo = 'docus'
+						%elif media['mediaType'] == 4:
+							%tipo = 'tv'
+						%end
+							<li><a href="{{tipo}}/{{media['idm']}}">{{media['name']}}</a></li>
 						%end
 					</ul>
 				%end
 		%end
 	</body>
 </html>
+
+
